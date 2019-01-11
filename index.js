794ef054b47e40d6ffdd4a32daa9051101fa672e
Sam Boylett <sam.boylett@netsells.co.uk>
2019-01-11 18:00:49 +0000

    working example

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+/*
+  String funcs I wanted to use:
+    String.prototype.replace
+    String.prototype.toLowerCase
+*/
 function isPalindrome(sentence) {
-  for (let i = 0; i < sentence.length / 2; i++) {
-    if (sentence[i] !== sentence.slice(-i - 1)[0]) return false;
+  const word = Array.from(sentence)
+    .filter(char => ![' ', ','].includes(char))
+    .map(char => {
+      const code = char.charCodeAt(0);
+
+      return String.fromCharCode(code >= 97 ? code - 32 : code);
+    })
+    .join('');
+
+  for (let i = 0; i < word.length / 2; i++) {
+    if (word[i] !== word.slice(-i - 1)[0]) return false;
   }
 
   return true;
--- index.test.js
+++ index.test.js
@@ -7,3 +7,26 @@ it('can identify an even palindrome', () => {
 it('can identify an odd palindrome', () => {
   expect(isPalindrome('abcdcba')).toBeTruthy();
 });
+
+it('can identify an even palindrome with a space', () => {
+  expect(isPalindrome('abcc ba')).toBeTruthy();
+});
+
+it('can identify an odd palindrome with a space', () => {
+  expect(isPalindrome('a bcdcba')).toBeTruthy();
+});
+
+it('can identify an even palindrome with a comma', () => {
+  expect(isPalindrome('abcc,ba')).toBeTruthy();
+});
+
+it('can identify an odd palindrome with a comma', () => {
+  expect(isPalindrome('a,bcdcba')).toBeTruthy();
+});
+
+it('passes the examples', () => {
+  expect(isPalindrome('A car, a man, a maraca')).toBeTruthy();
+  expect(isPalindrome('Adam is the best')).toBeFalsy();
+  expect(isPalindrome('A but tuba')).toBeTruthy();
+  expect(isPalindrome('I can say but tuba with a straight face')).toBeFalsy();
+});
