2ca2be82ada0c55d27183336b17f82fed2063dbb
Sam Boylett <sam.boylett@netsells.co.uk>
2019-03-26 16:31:44 +0000

    Add base

---
+++ rainbow-buttons/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <rainbow-buttons />
+  </div>
+</template>
+
+<script>
+import RainbowButtons from './components/RainbowButtons.vue'
+
+export default {
+  name: 'app',
+  components: {
+    RainbowButtons
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ rainbow-buttons/src/components/RainbowButtons.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <button
+      v-for="backgroundColor in colours"
+      :key="backgroundColor"
+      :style="{ backgroundColor }"
+    >
+      {{ backgroundColor }}
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'RainbowButtons',
+
+  computed: {
+    colours() {
+      return ['#DD9900', '#00AAFF'];
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
