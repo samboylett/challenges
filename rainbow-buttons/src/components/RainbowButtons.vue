2411ce7b6ed43c5589207b891db723de606a6e51
Sam Boylett <sam.boylett@netsells.co.uk>
2019-03-27 09:22:02 +0000

    Do challenge

--- rainbow-buttons/src/components/RainbowButtons.vue
+++ rainbow-buttons/src/components/RainbowButtons.vue
@@ -5,7 +5,7 @@
       :key="backgroundColor"
       :style="{ backgroundColor }"
     >
-      {{ backgroundColor }}
+      Link
     </button>
   </nav>
 </template>
@@ -14,9 +14,50 @@
 export default {
   name: 'RainbowButtons',
 
+  props: {
+    start: {
+      type: Number,
+      default: 0xDD9900,
+    },
+
+    end: {
+      type: Number,
+      default: 0x00AAFF,
+    },
+
+    steps: {
+      type: Number,
+      default: 7,
+    },
+  },
+
   computed: {
     colours() {
-      return ['#DD9900', '#00AAFF'];
+      const start = this.getRGB(this.start);
+      const end = this.getRGB(this.end);
+      const colours = [];
+
+      for (let i = 1; i <= this.steps; i++) {
+        const colour = [];
+
+        for (let c = 0; c < start.length; c++) {
+          colour.push(start[c] + ((end[c] - start[c]) * (i / this.steps)));
+        }
+
+        colours.push(colour);
+      }
+
+      return colours.map(([r, g, b]) => `rgb(${ r }, ${ g }, ${ b })`);
+    },
+  },
+
+  methods: {
+    getRGB(hex) {
+      return [
+        (hex >> 16) & 0xFF,
+        (hex >> 8) & 0xFF,
+        hex & 0xFF,
+      ];
     },
   },
 }
